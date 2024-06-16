713e08740927fbe9c393cc45337869ffb47b583e
nihal666 <nihaldas666@gmail.com>
2024-06-16 14:05:10 +0530

    fi

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
+    const { id, email_addresses, image_url, first_name, last_name } =
       evt.data;
 
     const user = {
